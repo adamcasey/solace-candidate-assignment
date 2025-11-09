1d3b129650e05e89712853727dc90bf573790224
Adam Casey <adam-casey@live.com>
2025-11-08 19:20:25 -0600

    add types folder with type definitions

---
+++ src/types/advocate.ts
@@ -0,0 +1,10 @@
+export type Advocate = {
+  id?: string | number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: string[];
+  yearsOfExperience: string | number;
+  phoneNumber: string;
+};
