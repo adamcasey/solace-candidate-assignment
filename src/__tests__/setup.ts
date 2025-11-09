3c8e98c47d633ce9fbe18cde124035b003f3bcfd
Adam Casey <adam-casey@live.com>
2025-11-09 17:26:11 -0600

    add config for testing

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
+import '@testing-library/jest-dom';
+
 config();
 
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL must be set for tests');
+if (process.env.NODE_ENV === 'test' && !process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL must be set for integration tests');
 }
